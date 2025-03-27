58f6b0a7e5616ea5e1531d4b992dc20a1779f8a8
freerossagora <freerossagora@tutanota.com>
2025-03-27 15:53:01 +0800

    test: update test title and remove redundant test case for adding new crypto currency account

--- playwrighttests/accounts.spec.ts
+++ playwrighttests/accounts.spec.ts
@@ -1,19 +1,12 @@
 import { test, expect } from '@playwright/test'
 
-test('has title', async ({ page, browserName }) => {
+test('add new crytpo currency account', async ({ page, browserName }) => {
+  // Navigate to the application
   await page.goto('http://localhost:1234/')
 
   // Increase timeout for slower browsers
   const timeout =
     browserName === 'webkit' || browserName === 'chromium' ? 15000 : 10000
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Haveno-Web/)
-})
-
-test('add new crytpo currency account', async ({ page }) => {
-  // Navigate to the application
-  await page.goto('http://localhost:1234/')
 
   // Wait for the menu button to be visible and click it to open the menu
   const menuButton = await page.waitForSelector('button.menu-btn', {
